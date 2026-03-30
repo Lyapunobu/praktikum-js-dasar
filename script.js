300ea2804c83e0918c4a291b0515bbd5f2581a97
Lyapunobu <anaqiahza2006@gmail.com>
2026-03-30 22:49:36 +0700

    Soal A1 done - FIX 'klik' to 'click'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navLinks = document.querySelectorAll('nav a');
 
 
 navLinks.forEach(function(link) {
-  link.addEventListener('klik', function(e) {
+  link.addEventListener('click', function(e) { // FIX - event listener diganti yang sebelumnya "klik" menjadi "click"
     e.preventDefault();
     console.log('Navigasi ke: ' + e.target.innerText);
   });
