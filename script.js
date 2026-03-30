e8439ac45785de3e4d08047427d37eadd7158115
Lyapunobu <anaqiahza2006@gmail.com>
2026-03-30 23:16:53 +0700

    Soal A3 start

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ const judul = document.querySelector('h1');
 
 judul.innerText = 'AI dan Masa Depan Teknologi Indonesia'; // ??? script jalan secara normal dan tidak ada error di console sebelum didebug
 
+// Soal A3
+const paragraphs = document.querySelectorAll('.main-article p');
+let totalKata = 0;
+
+paragraphs.forEach(function(p) {
+  const kata = p.innerText.split(' ');
+  totalKata = kata.length;
+});
+
+console.log('Total kata: ' + totalKata);
